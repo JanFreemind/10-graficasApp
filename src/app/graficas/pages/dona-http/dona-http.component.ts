b529655727c7eb9fdbc904d2ef4b1e59949fd3ae
JanPLosa <jplosa@tlsi.es>
2022-04-28 12:02:31 +0200

    Mostrar la informacion en la gráfica

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,6 +10,23 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  public doughnutChartLabels: string[] = [ 
+    //'Download Sales', 'In-Store Sales', 'Mail-Order Sales' 
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { 
+        data: [ 
+          //350, 450, 100, 70 
+        ],
+        backgroundColor: ['#5351E8', '#5120D4', '#204FD4', '#2593F6'  ] 
+      },
+    ]
+  };
+
+  public doughnutChartType: ChartType = 'doughnut';
+
   constructor( private graficasService: GraficasService) { }
 
   ngOnInit(): void {
@@ -16,6 +34,11 @@ export class DonaHttpComponent implements OnInit {
     this.graficasService.getUsuariosRedesSociales()
       .subscribe( data => {
         console.log(data);
+        const labels = Object.keys( data );
+        const values = Object.values( data );
+
+        this.doughnutChartData['labels'] = labels;
+        this.doughnutChartData['datasets'][0].data = values;
       });
 
   }
