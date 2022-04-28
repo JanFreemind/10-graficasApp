81eb947f82c5d7a7daf117c476ad8c46f94d7a1c
JanPLosa <jplosa@tlsi.es>
2022-04-28 12:19:54 +0200

    Cambiar informacion mediante rxjs

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -31,15 +31,23 @@ export class DonaHttpComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.graficasService.getUsuariosRedesSociales()
-      .subscribe( data => {
-        console.log(data);
-        const labels = Object.keys( data );
-        const values = Object.values( data );
+    // this.graficasService.getUsuariosRedesSociales()
+    //   .subscribe( data => {
+    //     console.log(data);
+    //     const labels = Object.keys( data );
+    //     const values = Object.values( data );
+
+    //     this.doughnutChartData['labels'] = labels;
+    //     this.doughnutChartData['datasets'][0].data = values;
+    //   });
+
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+      .subscribe( ({ labels, values}) => {
 
         this.doughnutChartData['labels'] = labels;
         this.doughnutChartData['datasets'][0].data = values;
-      });
+
+      })
 
   }
 
